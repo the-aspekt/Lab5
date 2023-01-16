cc2996e2f3a57df82f33b50d6b133bab6133d7cf
Pavel <sozonovps@gmail.com>
2023-01-16 19:33:19 +0800

    task 1 complete

--- Program.cs
+++ Program.cs
@@ -10,16 +10,17 @@ namespace Lab5
     {
         static void Main(string[] args)
         {
-            const int dimension = 6;
+            const int dimension = 7;
+            Console.WriteLine("Введите массив из {0} элементов", dimension);
             int[] array = new int[dimension];
             int sum = 0;
-            for (int i = 0; i <= dimension; i++)
+            for (int i = 0; i < dimension; i++)
             {
                 array[i] = Convert.ToInt32(Console.ReadLine());
                 sum += array[i];
             }
-            Console.WriteLine((double)sum/(dimension+1)); 
-            Console.ReadKey();
+            Console.WriteLine("Среднее арифметическое введенных значений равно {0}",(double)sum/(dimension)); 
+            Console.ReadLine();
 
         }
     }
