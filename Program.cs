060608f03a6fe292e4e1da0416ba444be097b197
Pavel <sozonovps@gmail.com>
2023-01-16 19:22:21 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int dimension = 6;
+            int[] array = new int[dimension];
+            int sum = 0;
+            for (int i = 0; i <= dimension; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+                sum += array[i];
+            }
+            Console.WriteLine((double)sum/(dimension+1)); 
+            Console.ReadKey();
+
+        }
+    }
+}
